18c3563a4834a8fef9f2386c987f7695706c68d5
dhacker29 <davidhackerdvm@gmail.com>
2012-10-24 13:05:52 -0500

    add full_scorpion_mini lunch combo

---
+++ AndroidProducts.mk
@@ -0,0 +1,1 @@
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_scorpion_mini.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_scorpion_mini-eng
-
+add_lunch_combo cm_scorpion_mini-userdebug
+add_lunch_combo full_scorpion_mini-eng
