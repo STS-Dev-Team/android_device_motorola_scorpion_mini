3de84a038f5f3e0e2e10140febd8db866a1616ee
dhacker29 <davidhackerdvm@gmail.com>
2012-11-16 20:40:30 -0600

    updates for 4.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,11 @@
 # inherit from the proprietary version
 -include vendor/motorola/scorpion_mini/BoardConfigVendor.mk
 
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 4294967296
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/scorpion_mini/bluetooth
+
 # Kernel
 #TARGET_PREBUILT_KERNEL := device/motorola/scorpion_mini/kernel
 
